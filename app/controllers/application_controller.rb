24bb351b8c2b85522b169cd48cd0935b8993d808
Cian Guinee <cian.guinee@gmail.com>
2021-10-01 03:13:12 +0100

    Add index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   def paginate(batch_size: 10)
-    @limit = params[:l] || batch_size
-    @offset = params[:o] || 0
+    @limit = (params[:l] || batch_size).to_i
+    @offset = (params[:o] || 0).to_i
+    @max_records = self.class.to_s.gsub('Controller', '').constantize.count
   end
 end
--- app/controllers/speed_test_controller.rb
+++ app/controllers/speed_test_controller.rb
@@ -1,10 +1,10 @@
 class SpeedTestController < ApplicationController
   def dashboard
-    @speed_tests = SpeedTest.where('created_at > ?', 1.year.ago)
+    @speed_tests = SpeedTest.where('created_at > ?', 1.year.ago).order(:created_at => :desc)
   end
 
   def index
-    paginate batch_size: 15
-    @speed_tests = SpeedTest.all.limit(@limit).offset(@offset)
+    paginate
+    @speed_tests = SpeedTest.all.limit(@limit).offset(@offset).order(:created_at => :desc)
   end
 end
